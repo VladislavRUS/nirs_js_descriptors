c715faf7e06c4fd31a29c6fccea1520d58cf2d5c
Владислав Курочкин <vlad555.95@mail.ru>
2017-12-10 18:04:21 +0400

    pca

--- Helpers.js
+++ Helpers.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const Jimp = require("jimp");
-const AFS = require('./AFS');
 
 const Helpers = {
     scaleImage: function (image, scaleFactor) {
@@ -36,18 +35,18 @@ const Helpers = {
         return image;
     },
 
-    matrix2image: function (matrix) {
-        const width = matrix.length;
-        const height = matrix[0].length;
+    matrix2image: function (matrix, name) {
+        const height = matrix.length;
+        const width = matrix[0].length;
 
         new Jimp(width, height, (err, image) => {
-            for (let i = 0; i < width; i++) {
-                for (let j = 0; j < height; j++) {
+            for (let i = 0; i < height; i++) {
+                for (let j = 0; j < width; j++) {
                     image.setPixelColor(matrix[i][j], j, i);
                 }
             }
 
-            image.write('test.jpg');
+            image.write(name + '.jpg');
         });
     },
     createMatrix: function (width, height, fillNumber) {
@@ -70,6 +69,9 @@ const Helpers = {
 
     image2Vector: function (image) {
         const gradient = this.getImageGradient(image);
+
+        //this.matrix2image(gradient.x, 'gradients/x' + Math.random() * 10000);
+        //this.matrix2image(gradient.y, 'gradients/y' + Math.random() * 10000);
 
         gradient.x = this.matrix2Vector(gradient.x);
         gradient.y = this.matrix2Vector(gradient.y);
@@ -112,48 +114,71 @@ const Helpers = {
     },
 
     getXGradient: function (image) {
-        const filter = [1, 0, -1];
+        const sobelOperatorX = [
+            [+1, +0, -1],
+            [+2, +0, -2],
+            [+1, +0, -1]
+        ];
+
         const matrix = image.matrix;
         let result = Helpers.createMatrix(image.width, image.height, 0);
 
-        for (let i = 0; i < image.height; i++) {
-            for (let j = 0; j < image.width; j++) {
+        for (let i = 1; i < image.height - 1; i++) {
+            for (let j = 1; j < image.width - 1; j++) {
 
-                result[i][j] = matrix[i]
-                    .slice(j, j + filter.length)
-                    .map((pixelValue, idx) => pixelValue *= filter[idx])
-                    .reduce((first, second) => first + second);
+                let rows = matrix.slice(i - 1, i + 2);
+                let window = rows.map(row => row.slice(j - 1, j + 2));
+
+                let arr = [];
+
+                window.forEach((row, rowIdx) => {
+                    row.forEach((value, columnIdx) => {
+                        arr.push(value * sobelOperatorX[rowIdx][columnIdx]);
+                    });
+                });
+
+                result[i][j] = arr.reduce((first, second) => first + second);
             }
         }
 
-        result = result.slice(1, result.length - 1);
-        result = result.map(row => row.slice(1, row.length - 1));
-
-        this.matrix2image(result);
+        result = result
+            .map(row => row.slice(1, row.length - 1))
+            .slice(1, result.length - 1);
 
         return result;
     },
 
     getYGradient: function (image) {
-        const filter = [1, 0, -1];
+        const sobelOperatorY = [
+            [+1, +2, +1],
+            [+0, +0, +0],
+            [-1, -2, -1]
+        ];
+
         const matrix = image.matrix;
         let result = Helpers.createMatrix(image.width, image.height, 0);
 
         for (let i = 1; i < image.height - 1; i++) {
-            for (let j = 0; j < image.width; j++) {
+            for (let j = 1; j < image.width - 1; j++) {
 
-                let current = matrix[i][j],
-                    prev = matrix[i - 1][j],
-                    next = matrix[i + 1][j];
+                let rows = matrix.slice(i - 1, i + 2);
+                let window = rows.map(row => row.slice(j - 1, j + 2));
 
-                result[i][j] = [current, prev, next]
-                    .map((pixelValue, idx) => pixelValue *= filter[idx])
-                    .reduce((first, second) => first + second);
+                let arr = [];
+
+                window.forEach((row, rowIdx) => {
+                    row.forEach((value, columnIdx) => {
+                        arr.push(value * sobelOperatorY[rowIdx][columnIdx]);
+                    });
+                });
+
+                result[i][j] = arr.reduce((first, second) => first + second);
             }
         }
 
-        result = result.slice(1, result.length - 1);
-        result = result.map((row => row.slice(1, row.length - 1)));
+        result = result
+            .map(row => row.slice(1, row.length - 1))
+            .slice(1, result.length - 1);
 
         return result;
     },
@@ -167,9 +192,7 @@ const Helpers = {
         });
     },
 
-    calcFeatures: function(vector, eigenVectors) {
-        const result = [];
-
+    calcFeatures: function (vector, eigenVectors) {
         let maxValue = Number.MIN_VALUE;
 
         eigenVectors.forEach(eigenVector => {
@@ -189,40 +212,97 @@ const Helpers = {
         for (let i = 0; i < N; i++) {
             let complexBaseVector = eigenVectors.map(eigenVector => eigenVector[i]);
 
-            featureVector[i] = new AFS(vector, complexBaseVector, maxValue, ortogonality);
+            featureVector[i] = this.AFS(vector, complexBaseVector, maxValue, ortogonality);
         }
 
-        return result;
+        return featureVector;
     },
 
-    AFS: function(complexVector, complexBaseVector, maxBaseValue, ortogonality) {
+    AFS: function (complexVector, complexBaseVector, maxBaseValue, ortogonality) {
+        const afsType = 2;
 
+        let sum = 0,
+            outputValue = 0;
+        let firstMaxBaseValue = 1 / maxBaseValue,
+            secondMaxBaseValue = Math.pow(firstMaxBaseValue, 2),
+            N = complexVector.length;
+
+        for (let i = 0; i < N; i++) {
+            let zBase = complexBaseVector[i],
+                z = complexVector[i];
+
+            let valBase = Helpers.getComplexModule(zBase),
+                val = Helpers.getComplexModule(z);
+
+            let angle = Helpers.getComplexAngle(z),
+                angleBase = Helpers.getComplexAngle(zBase);
+
+            let diff = Helpers.toDegrees(angle) - Helpers.toDegrees(angleBase);
+
+            diff = diff < -180 ?
+                diff + 180 :
+                diff - 180;
+
+            let angleCoef = ortogonality ?
+                0.5 * (1 + Math.cos(diff)) :
+                Math.abs(Math.cos(diff));
+
+            switch (afsType) {
+                case 1:
+                    {
+                        val *= valBase * firstMaxBaseValue;
+                        break;
+                    }
+                case 2:
+                    {
+                        val *= valBase * valBase * secondMaxBaseValue;
+                        break;
+                    }
+            }
+
+            outputValue += angleCoef * val;
+            sum += val;
+        }
+
+        return +(outputValue / sum).toFixed(4);
     },
 
-    getComplexModule: function(complexValue) {
+    getComplexModule: function (complexValue) {
         return Math.sqrt(Math.pow(complexValue.r, 2) + Math.pow(complexValue.i, 2));
     },
 
-    getComplexAngle: function(complexValue) {
-        return Math.atan(complexValue.i / complexValue/r);
+    getComplexAngle: function (complexValue) {
+        return Math.atan(complexValue.i / complexValue.r);
     },
 
-    toDegrees: function(radians) {
+    toDegrees: function (radians) {
         return radians * 180 / Math.PI;
     },
 
-    addComplexValues: function(first, second) {
+    addComplexValues: function (first, second) {
         return {
             r: first.r + second.r,
             i: first.i + second.i
         }
     },
 
-    multiplyComplexValues: function(first, second) {
+    multiplyComplexValues: function (first, second) {
         return {
             r: first.r * second.r - first.i * second.i,
             i: first.r * second.i + first.i * second.r
         }
+    },
+
+    countErros: function (classes, predicted) {
+        let errors = 0;
+
+        classes.forEach((cl, idx) => {
+            if (cl !== predicted[idx]) {
+                errors++;
+            }
+        });
+
+        return errors;
     }
 };
 
--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const Jimp = require("jimp");
 const Loader = require('./Loader');
 const PCA = require('./PCA');
+const KNN = require('ml-knn');
+const Helpers = require('./Helpers');
 
 const baseDir = './CroppedYale';
 
@@ -16,10 +18,33 @@ const trainParams = {
     }
 };
 
+const testParams = {
+    class: {
+        start: 0,
+        end: 5
+    },
+    samples: {
+        start: 5,
+        end: 10
+    }
+};
+
 const lambda = 0.95;
 const featuresNumber = 4;
 
 const trainingData = Loader.loadData(baseDir, trainParams);
 const PCAResult = PCA.get(trainingData.vectors, lambda, featuresNumber);
 
-PCA.apply(trainingData.vectors, PCAResult.eigenVectors, PCAResult.meanVector);
+// KNN
+const trainFeatures = PCA.apply(trainingData.vectors, PCAResult.eigenVectors, PCAResult.meanVector);
+const knn = new KNN(trainFeatures, trainingData.classes);
+
+const testData = Loader.loadData(baseDir, testParams);
+const testFeatures = PCA.apply(testData.vectors, PCAResult.eigenVectors, PCAResult.meanVector);
+
+let predictions = knn.predict(testFeatures);
+
+let errors = Helpers.countErros(trainingData.classes, predictions) 
+let probability = errors / trainingData.classes.length;
+
+console.log(errors, 1 - probability);
